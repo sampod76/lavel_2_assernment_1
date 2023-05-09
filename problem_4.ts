a513f536c0a57729b3ba56c13fd5969983772536
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-05-09 22:34:06 +0600

    fixed privet problem

--- problem_4.ts
+++ problem_4.ts
@@ -3,33 +3,33 @@
 Create a TypeScript class called Student that extends the Person class and has an additional private property grade. Define a public method getGrade that returns the student's grade. Use the super keyword to call the constructor of the Person class and initialize the name and age properties. */
 
 class Person {
-    name: string;
-    age: number;
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age
-    }
-
-    getDetails(): string {
-        return `name:${this.name} age:${this.age}`
-    }
-
-}
-
-class Student extends Person {
-    private grade: string;
-    constructor(name: string, age: number, grade: string) {
-        super(name, age)
-        this.grade = grade
-    };
-    public getGrade(): string {
-        return this.grade
-    }
-
-}
-
-const MyInfo = new Person("sampod",35)
-console.log(MyInfo.getDetails());
-
-const MyFriendInfo = new Student("akash",12,"A+")
-console.log(MyFriendInfo.getGrade());
+    private name: string;
+    private age: number;
+     constructor(name: string, age: number) {
+         this.name = name;
+         this.age = age
+     }
+ 
+     getDetails(): string {
+         return `name:${this.name} age:${this.age}`
+     }
+ 
+ }
+ 
+ class Student extends Person {
+     private grade: string;
+     constructor(name: string, age: number, grade: string) {
+         super(name, age)
+         this.grade = grade
+     };
+     public getGrade(): string {
+         return this.grade
+     }
+ 
+ }
+ 
+ const MyInfo = new Person("sampod",35)
+ console.log(MyInfo.getDetails());
+ 
+ const MyFriendInfo = new Student("akash",12,"A+")
+ console.log(MyFriendInfo.getGrade());
